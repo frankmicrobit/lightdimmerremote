83275e8f2f2fec50e534b25fa76c941438defbf3
frankmicrobit <100852447+frankmicrobit@users.noreply.github.com>
2022-03-23 12:55:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,49 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(0)
+    Wait = true
+    Threshold += -5
+    basic.showNumber(Threshold)
+    Wait = false
 })
-input.onSound(DetectedSound.Loud, function () {
-    SoudLevel = input.soundLevel()
-    if (SoudLevel > 100) {
-        radio.sendNumber(Brightnes)
+input.onButtonPressed(Button.AB, function () {
+    Wait = true
+    RespondOnSound = !(RespondOnSound)
+    if (RespondOnSound) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
     }
+    basic.pause(500)
+    basic.clearScreen()
+    Wait = false
+})
+input.onButtonPressed(Button.B, function () {
+    Wait = true
+    Threshold += 5
+    basic.showNumber(Threshold)
+    Wait = false
 })
 let SoudLevel = 0
-let Brightnes = 0
-Brightnes = 1000
+let Wait = false
+let RespondOnSound = false
+let Threshold = 0
+led.setBrightness(10)
+Threshold = 120
+RespondOnSound = false
+let Brightnes = 1000
 radio.setGroup(1)
+Wait = false
 basic.forever(function () {
-	
+    if (!(Wait)) {
+        SoudLevel = input.soundLevel()
+        if (SoudLevel > Threshold) {
+            if (RespondOnSound) {
+                radio.sendNumber(Brightnes)
+            }
+            basic.showNumber(SoudLevel)
+        }
+        led.plotBarGraph(
+        SoudLevel,
+        Threshold
+        )
+    }
 })
